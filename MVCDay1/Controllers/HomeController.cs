74f4b7e94fd230b6adaa9498dd8cd37951de8a07
LindaMoussa <linda.moussa.95@gmail.com>
2020-05-04 01:14:41 +0200

    day1

---
+++ MVCDay1/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDay1.Controllers
+{
+    public class User
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ViewResult Index()
+        {
+            return View(); // class name
+        }
+        [HttpGet]
+        public ViewResult Form()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult Form(User u)
+        {
+            if (u.name != null && u.email != null )
+            {
+                ViewBag.Name = u.name;
+                return View("welcome"); // class name
+            }
+            return View();
+        }
+    }
+}
